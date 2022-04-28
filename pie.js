b435e9c77cf6cd7d8ed2de77085bf333e6483e02
Rerium <rer.of.riverwood@gmail.com>
2022-04-29 01:16:39 +0300

    Переписал информационный блок на JSON

--- pie.js
+++ pie.js
@@ -1,56 +1,82 @@
+let menu = document.querySelector('.sunrayGood');
+let infoContent = document.querySelector('.infoBlock');
+let content = document.getElementById("contentText");
+let items = menu.querySelectorAll('.sunray');
+let button = menu.querySelector('.sunrayBtn');
+let active = false;
 
-    let menu = document.querySelector('.sunrayGood');
-    let items = menu.querySelectorAll('.sunray');
-    let button =  menu.querySelector('.sunrayBtn');
-    let active = false;
+var jsonSunArray;
 
-    const length = items.length;
-    const arc = 2 * Math.PI * (1/10);
-    const radius = 20;
-    let size
-    if(window.innerWidth > window.innerHeight){
-        size = Math.round(window.innerHeight * 0.7)
-        console.log("width");
-    } else{
-        size = Math.round(window.innerWidth * 0.7)
-        console.log("height");
+
+const length = items.length;
+const arc = 2 * Math.PI * (1 / 10);
+const radius = 20;
+let size
+
+if (window.innerWidth > window.innerHeight) {
+    size = Math.round(window.innerHeight * 0.7)
+} else {
+    size = Math.round(window.innerWidth * 0.7)
+}
+menu.style.width = size + "px";
+menu.style.height = size + "px";
+
+//Отрисовка "лучей"
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    active = !active;
+
+    if (active) {
+        button.classList.add('sunrayBtnActive');
+        for (let i = 0; i < length; i++) {
+            let x, y;
+            if (i < 10) {
+                const angel = i * arc;
+                x = radius * Math.cos(angel);
+                y = radius * Math.sin(angel);
+            }
+            if (i >= 10 && i < 30) {
+                const angel = i * arc / 2;
+                x = radius * Math.cos(angel) * 2;
+                y = radius * Math.sin(angel) * 2;
+            }
+            if (i >= 30 && i < 40) {
+                const angel = i * arc / 2;
+                x = Math.round(radius * Math.cos(angel) * 2.5);
+                y = Math.round(radius * Math.sin(angel) * 2.5);
+            }
+            items[i].style.left = 50 + x + "%";
+            items[i].style.top = 50 + y + "%";
+        }
+    } else {
+        button.classList.remove('sunrayBtnActive');
+        for (let i = 0; i < length; i++) {
+            items[i].removeAttribute('style');
+        }
+    };
+
+});
+
+// создание JSON
+let requestURL = "http://127.0.0.1:5500/zabota/pie.json"; // ссылка на внешний JSON
+
+var request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    jsonSunArray = request.response;
+}
+window.addEventListener("mousedown", (e) => {
+
+    if (e.target.classList == "sunray") {
+        let currentWindow = jsonSunArray[e.target.id];
+        console.log(currentWindow);
+        content.style.display = "flex";
+        $(infoContent).append('<img src=' + currentWindow.img1+ '><img src='+currentWindow.img2+ '><p>'+currentWindow.leading+currentWindow.text+'</p>');
     }
-    menu.style.width = size + "px";
-    menu.style.height = size + "px";
-     
-
-    button.addEventListener( 'click' , (e) =>{
-        e.preventDefault();
-
-        active = !active;
-
-        if(active){
-            button.classList.add('sunrayBtnActive');
-            for (let i = 0; i < length; i++) {
-                let x, y;
-                if (i < 10){
-                    const angel = i * arc;
-                    x = radius * Math.cos(angel);
-                    y = radius * Math.sin(angel);
-                }
-                if (i >= 10 && i < 30){
-                    const angel = i * arc/2;
-                    x = radius * Math.cos(angel)*2;
-                    y = radius * Math.sin(angel)*2;
-                }
-                if (i >= 30 && i < 40){
-                    const angel = i * arc/2;
-                    x = Math.round(radius * Math.cos(angel)*2.5);
-                    y = Math.round(radius * Math.sin(angel)*2.5);
-                }
-                items[i].style.left = 50 + x + "%";
-                items[i].style.top = 50 + y + "%";
-            }
-        }else{
-            button.classList.remove('sunrayBtnActive');
-            for (let i=0; i < length; i++){
-                items[i].removeAttribute('style');
-            }
-        };
-
-    });
+    if (e.target.id == "contentText") {
+        content.style.display = "none";
+    }
+});
