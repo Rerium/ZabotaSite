4a75c3cf8141018db0e4b57a16ff0851907e849f
Rerium <rer.of.riverwood@gmail.com>
2022-05-28 01:44:00 +0300

    preprod version

--- JS/vacancies.js
+++ JS/vacancies.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $('.titel').click(function (event) {
-        if($('.content').hasClass('one')){
+        if($('.vacancys').hasClass('one')){
             console.log("t")
             $('.titel').not($(this)).removeClass('active');
-            $('.discription').not($(this).next()).slideUp(300);
+            $('.text').not($(this).next()).slideUp(300);
         }
         $(this).toggleClass('active').next().slideToggle(300);
     })
